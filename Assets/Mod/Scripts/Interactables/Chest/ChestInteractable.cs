774f7820833a7f24f60935a2cdefe9ca891594a7
RodgerChou <gailichv@yandex.ru>
2020-06-14 16:07:27 +0300

    Init
    Initial commit

---
+++ Assets/Mod/Scripts/Interactables/Chest/ChestInteractable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestInteractable : Interactable
+{
+    public GameObject loot;
+    public Transform lootPawnPosition;
+    private bool opened = false;
+
+    override public void PerformAction()
+    {
+        if (!opened)
+        {
+            opened = true;
+            interactable = false;
+
+            var spawnedLoot = Instantiate(loot, lootPawnPosition.position, Quaternion.identity);
+            var rigidBody = spawnedLoot.GetComponent<Rigidbody>();
+            var force = new Vector3(Random.Range(-100, 100), 300, Random.Range(-150, -100));
+            rigidBody.AddForce(force);
+
+            base.PerformAction();
+        }
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (!opened)
+        {
+            base.OnTriggerEnter(collision);
+        }
+    }
+}
---
+++ Assets/Mod/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public Material usualMaterial;
+    public Material activeMaterial;
+
+
+    private Animation anim;
+    public bool interactable = false;
+    private bool locked = false;
+
+    private void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    private void Update()
+    {
+        Debug.Log("update");
+        if (interactable)
+        {
+            var forwardVector = Camera.main.transform.forward;
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.transform.position, forwardVector, out hit, 5))
+            {
+                if (hit.collider.gameObject.tag == "Interactable")
+                {
+                    FeedbackFlashHUD.instance.SetCanInteract(true);
+                    if (Input.GetKeyDown(KeyCode.F))
+                    {
+                        Debug.Log("Key pressed");
+                        PerformAction();
+                    }
+                }
+            } else
+            {
+                FeedbackFlashHUD.instance.SetCanInteract(false);
+            }
+
+
+        }
+    }
+
+    virtual public void PerformAction()
+    {
+        anim.Play();
+        FeedbackFlashHUD.instance.SetCanInteract(false);
+    }
+
+    public void PlayAnimation(string animName)
+    {
+        anim.Play(animName);
+    }
+
+    virtual public void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Collistion enter");
+        if (collision.gameObject.tag == "Player")
+        {
+            interactable = true;
+        }
+    }
+
+    virtual public void OnTriggerExit(Collider collision)
+    {
+        Debug.Log("Collistion exit");
+        if (collision.gameObject.tag == "Player")
+        {
+            interactable = false;
+            FeedbackFlashHUD.instance.SetCanInteract(false);
+        }
+    }
+}
