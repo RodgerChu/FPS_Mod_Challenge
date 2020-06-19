8c4e00cc3041478cd0a3c5e0804c5218acef62b1
Qscef <vasenkov_a_a@mail.ru>
2020-06-20 02:09:18 +0300

    add animation light_fly

---
+++ Assets/Mod/Scripts/Animations/animation_light_fly.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation_light_fly : MonoBehaviour
+{  
+    void Awake()
+    {
+        StartCoroutine(idle());
+    }
+
+    void Update()
+    {
+
+    }
+
+    private IEnumerator idle() {
+        while (true)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.005f, this.transform.position.z);
+                yield return new WaitForSeconds(.02f);
+            }
+
+            for (int i = 0; i < 30; i++)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.005f, this.transform.position.z);
+                yield return new WaitForSeconds(.02f);
+            }
+        }
+    }
+}
